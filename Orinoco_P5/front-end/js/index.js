37eb803067c9de7dd20f0e6ba9540e987e9e5f6e
Tchessi Pre <tchesspreza@yahoo.fr>
2021-08-10 10:44:01 +0200

    Appel API correction erreur

--- Orinoco_P5/front-end/js/index.js
+++ Orinoco_P5/front-end/js/index.js
@@ -1,17 +1,18 @@
 main()
 
-function main(){
-  const articles = getArticles()
+async function main(){
+  const articles = await getArticles()
+  console.log(articles)
   displayArticles(articles)
 }
 // Appel API
 function getArticles(){
-  fetch("http://localhost:3000/api/teddies")
+  return fetch("http://localhost:3000/api/teddies")
   .then(function(httpBobyResponse) {
     return httpBobyResponse.json( )
   })
   .then(function(articles){
-    console.log(articles)
+    return articles 
   })
   .catch(function(error) {
     alert(error)
