e1bc63c7a6cb3de06bc86fab309466c3d6c2b3ea
Tchessi Pre <tchesspreza@yahoo.fr>
2021-08-11 09:32:48 +0200

    Update index.js

--- Orinoco_P5/front-end/js/index.js
+++ Orinoco_P5/front-end/js/index.js
@@ -1,24 +1,41 @@
 main()
 
 async function main(){
-  const articles = await getArticles()
-  console.log(articles)
-  displayArticles(articles)
+const articles = await getArticles()
+for (article of articles) {
+  displayArticles(article)
+  console.log(article)
+} 
 }
 // Appel API
 function getArticles(){
-  return fetch("http://localhost:3000/api/teddies")
-  .then(function(httpBobyResponse) {
-    return httpBobyResponse.json( )
-  })
-  .then(function(articles){
-    return articles 
-  })
-  .catch(function(error) {
-    alert(error)
-  })
+return fetch("http://localhost:3000/api/teddies")
+.then(function(httpBobyResponse) {
+  return httpBobyResponse.json( )
+})
+.then(function(articles){
+  return articles 
+})
+.catch(function(error) {
+  alert(error)
+})
+}
+// Affichage des articles 
+function displayArticles(article) {
+document.getElementById("main").innerHTML += `   
+<div class="col">
+  <div class="card">
+    <img src="${article.imageUrl}" class="card-img-top" id="imageUrl" width="400" height="300" alt="...">
+    <div class="card-body" >
+    <h5 class="produitName" id="name">${article.name}</h5>
+    <h5 class="produitPrix" id="price">${article.price}€</h5>
+  </div>
+  <p id="description">${article.description}</p>
+</div>`
+
+document.getElementById("produitLink").href += `?id=${article.id}`
+
+
 }
 
-function displayArticles() {
-  return ""
-}
+
