ce25f5e70578d70f48cda90401a550be12b5cd36
Tchessi Pre <tchesspreza@yahoo.fr>
2021-08-12 15:12:30 +0200

    Update produit.js

--- Orinoco_P5/front-end/js/produit.js
+++ Orinoco_P5/front-end/js/produit.js
@@ -24,9 +24,22 @@ async function main() {
     const article = await getArticle()
     console.log(article)
 
-    document.getElementById("product-image").src = article.imageUrl;
-    document.getElementById("produit-name").textContent = article.name;
-
+    document.getElementById("main").innerHTML = `
+<div class="card-body">        
+    <h5 class="produitName">${article.name}</h2>
+    <h5 class="produitPrix">${article.price/100}€</h5>
+</div> 
+        <div class="col-6">
+            <figure class="figure">
+                <img id="product-image" src="${article.imageUrl}" width="700" height="400" class="figure-img img-fluid rounded">
+            </figure>
+        </div>
+        <div class="col-6">
+        <p id="description">${article.description}</p>
+        <div class="col align-self-end">
+        <button type="button" class="btn btn-primary">Ajouter au panier</button>
+        </div>
+        </div>`
 
 }
 
